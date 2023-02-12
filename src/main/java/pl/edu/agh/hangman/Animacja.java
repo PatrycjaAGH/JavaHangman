8110ff65690370749104b045604d7d6c7b6b4f61
student74 <student74@pcoip.ki.agh.edu.pl>
2023-02-12 14:54:21 +0100

    hangman display

---
+++ src/main/java/pl/edu/agh/hangman/Animacja.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.hangman;
+
+import static pl.edu.agh.hangman.Hangman.HANGMANPICS;
+
+public class Animacja {
+
+    public void print() {
+        System.out.println(HANGMANPICS[0]);
+    }
+}
